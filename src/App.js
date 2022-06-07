fc4933744adb173a3ebe8b49df2aa939f1cc72b0
urvi004 <urvisharma54@gmail.com>
2022-06-07 20:52:47 +0530

    deploy form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import React , {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [name, setName] = useState();
+  const [lastName, setLastName] = useState();
+  const [fullName, setFullName] = useState();
+  
+
+  const onSubmits = (event) => {
+    event.preventDefault();
+       setFullName(name + " " + lastName);
+};
+
+const inputEvent2 = (event) => {
+  console.log(event.target.value);
+  setLastName(event.target.value);
+};
+
+  const inputEvent = (event) => {
+      console.log(event.target.value);
+      setName(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={onSubmits}>
+    <div >
+      <h1>Hello {fullName}</h1>
+      <input 
+             type="text" 
+             placeholder='enter your name' 
+             onChange={inputEvent}
+             value={name}
+             />
+
+          <br/>
+
+      <input 
+             type="text" 
+             placeholder='password' 
+             onChange={inputEvent2}
+             value={lastName}
+             />
+      <button type= "submit">click me</button>
+     
+    </div>
+    </form>
     </div>
   );
 }
