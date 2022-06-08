4fc0843111d74f7fb3419d1e23b9d3d7354282e9
urvi004 <urvisharma54@gmail.com>
2022-06-08 21:29:33 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -6,45 +6,63 @@ import './App.css';
 
 function App() {
 
-  const [name, setName] = useState();
-  const [lastName, setLastName] = useState();
-  const [fullName, setFullName] = useState();
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
   
-
-  const onSubmits = (event) => {
-    event.preventDefault();
-       setFullName(name + " " + lastName);
-};
-
-const inputEvent2 = (event) => {
-  console.log(event.target.value);
-  setLastName(event.target.value);
-};
-
   const inputEvent = (event) => {
       console.log(event.target.value);
-      setName(event.target.value);
-  };
+      console.log(event.target.name);
+
+      const name = event.target.name;
+      const value  = event.target.value;
+      
+      setFullName((preValue) => {
+        if (name=== 'fName'){
+          return{
+            fName: value,
+          lName: preValue.lName,
+          };
+          
+        } else if (name==='lName'){
+          return{
+            fName: preValue.fName,
+            lName: value,
+          }
+        }
+        //console.log(preValue);
+      });
+
+   };
+
+   const onSubmits = (event) => {
+   event.preventDefault();
+   alert('form submitted')
+   };
+
 
   return (
     <div className="App">
     <form onSubmit={onSubmits}>
     <div >
-      <h1>Hello {fullName}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <input 
              type="text" 
+             name= "fName"
              placeholder='enter your name' 
              onChange={inputEvent}
-             value={name}
+             value={fullName.fName}
              />
 
           <br/>
 
       <input 
              type="text" 
-             placeholder='password' 
-             onChange={inputEvent2}
-             value={lastName}
+             name='lName'
+             placeholder='enter your last name' 
+             onChange={inputEvent}
+             value={fullName.lName}
              />
       <button type= "submit">click me</button>
      
